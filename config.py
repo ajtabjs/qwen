73d59900561f198e00a8c5766b19bf1c20f2a4d5
aj <134237782+ajtabjs@users.noreply.github.com>
2026-07-21 14:42:02 -0700

    update config

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ CONFIG = {
     'min_confidence_threshold': 0.0,
     'max_acceptable_spread': 30.0,
     'min_acceptable_confidence': 0.40,
-    'captcha_capture_mode': 'auto',  # auto | src | screenshot
+    'captcha_capture_mode': 'screenshot',  # auto | src | screenshot
 
     # Drag behavior
     'drag_dead_zone': 96.0,
